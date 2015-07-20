dad5b7ca4c6177ae74b4c9943b84675883e9f4ff
Sergey Romanov <xxsmotur@gmail.com>
2015-07-20 23:22:42 +0500

    Add restrictions

--- src/bilint.js
+++ src/bilint.js
@@ -1,6 +1,11 @@
 
 //Q** - is triplet of points at the (x,y, result)
 export default function bilint(x,y, Q11, Q12, Q21, Q22){
+    if(Q11.length != 3 || Q12.length != 3 || Q21.length != 3 || Q22.length != 3){
+        console.error("Parameters Q11,  Q12, Q21 and Q22, must contain array of 3 items");
+        return 0;
+    }
+
     let x1 = Q11[0];
     let y1 = Q11[1];
     let result1 = Q11[2];
@@ -12,6 +17,11 @@ export default function bilint(x,y, Q11, Q12, Q21, Q22){
     let result3 = Q21[2];
 
     let result4 = Q22[2];
+
+    if(x1 != Q12[0] || y1 != Q21[1] || y2 != Q22[1] || Q21[0] != Q22[0]){
+        console.error("At least one point is contain error");
+        return 0;
+    }
 
     return (result1 * (x2 - x)* (y2 - y) + result2 * (x - x1) * (y2 - y) + result3 * (x2 - x) * (y - y1) + 
     result4 * (x - x1) * (y - y1)) * (1/((x2 - x1) * (y2 - y1)));
